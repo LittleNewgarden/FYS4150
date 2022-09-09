d720bce87c02862d871f5d21106dc3bad363406d
NoName1030 <{ID}+{username}@users.noreply.github.com112510875+NoName1030@users.noreply.github.com>
2022-09-09 20:53:58 +0200

    ?

--- oppgave7.py
+++ oppgave7.py
@@ -2,34 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# with open("u_x_output.txt", "r+") as file:
-#     words = []
-#     for line in file:
-#         words.append([float(line.split()[0]), float(line.split()[1])])
-        
-    
-#     words = (np.array(words))
-#     print(words)
-
-# plt.plot(words[:,0],words[:,1])
-
-# plt.show()
-
-
+# exact solution of u(x)
 def u(x):
     return 1- (1 - np.exp(-10))*x - np.exp(-10*x)
 
+# reading files produced in oppgave7.cpp, only taking up to n=10^4
 x = []
 v = []
-n = [10,100,1000,100000]
+n = [10,100,1000,10000]
 for i in n:
     x.append(np.loadtxt(f"P7_{i}.txt",unpack=True)[0])
     v.append(np.loadtxt(f"P7_{i}.txt",unpack=True)[1])
 
+# plotting the approximation to u(x) together with u(x)
+
 plt.plot(x[0],v[0],label='10 steps')
 plt.plot(x[1],v[1],label='100 steps')
 plt.plot(x[2],v[2],label='1000 steps')
-plt.plot(x[3],v[3],label='100000 steps')
+plt.plot(x[3],v[3],label='10000 steps')
 plt.plot(x[3],u(x[3]), label='u(x)')
 plt.legend()
+plt.savefig("figure_task_7.pdf")
+plt.xlabel('x')
+plt.ylabel('v(x) and g(x)')
 plt.show()
